e62a68320784bdcbee09d941182ba3c2d81e83b0
KQanother <hotpotisreal@gmail.com>
2017-02-10 17:55:17 +0800

    zyy

--- app/pages/account/login.js
+++ app/pages/account/login.js
@@ -1,3 +1,49 @@
 Page({
+data:{
+        phoneValue:'',
+        yzmValue:'',
+        disabled:false,
+        showClose:'none',
+        showActive:'none',
+        showGrey:'',
+        showPswdsrc:'../../images/loginbg-close.png',
+        src:'../../images/close.png',
+      loginHead:'../../images/login-head.png'
+    },
+    checkPhone:function(e) {
+       var value = e.detail.value;
+        var isPhone =/^(13[0-9]{9})$|^(14[0-9]{9})$|^(15[0-9]{9})$|^(18[0-9]{9})$|^(17[0-9]{9})$/;
+        if(value!=""){
+             this.setData({
+                showClose:''
+            })
+        }
+        else{
+             this.setData({
+                showClose:'none'
+            })
+        }
+        if(isPhone.test(value)){
+                this.setData({
+                    receive:'none',
+                    receivea:'',
+                    showGrey:'none',
+                    showActive:'',
+                    phoneValue:value                  
+                })
 
+        }
+        else{
+            this.setData({
+                showGrey:'',
+                showActive:'none'
+            })
+        }
+    },
+    clearPhone:function() {
+        this.setData({
+            phoneValue:'',
+            showClose:'none'
+        })
+    }
 });
